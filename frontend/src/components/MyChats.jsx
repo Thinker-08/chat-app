fd9e98f04cf201d65f533ed3517a7692390a9aeb
MIHIR AGRAWAL <agarwalmihir08@gmail.com>
2022-10-01 00:47:56 +0530

    Search bar and stuff done

--- frontend/src/components/MyChats.jsx
+++ frontend/src/components/MyChats.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { useToast } from '@chakra-ui/react';
+import axios from 'axios';
+import React, { useState } from 'react'
+import { ChatState } from '../Context/ChatProvider'
 
 const MyChats = () => {
+  const [loggedUser,setLoggedUser] = useState()
+  const {selectedChat,setSelectedChat,user, chats,setChats} = ChatState();
+  const toast = useToast();
+  const fetchChats=async()=>{
+    try{
+      const config={
+        Authorization:`Bearer ${user.token}`
+      }
+      const {data} = await axios.get("/api/chat",config);
+      setChats(data);
+    }catch(err){
+      toast({
+        title:"Error Occured",
+        description:"Failed to Load Chat",
+        status:"error",
+        
+      })
+    }
+  }
   return (
     <div>
       Hi These are your chats!!
--- frontend/src/components/chats.jsx
+++ frontend/src/components/chats.jsx
@@ -9,8 +9,8 @@ const Chats = () => {
   const {user} = ChatState();
   return (
     <div style={{width:'100%'}}>
-      
-      <Box d='flex' justifyContent='space-between' w='100%' h='91.5vh' p='10px'>
+      <SideDrawer/>
+      <Box display='flex' justifyContent='space-between' w='100%' h='91.5vh' p='10px'>
         {user && <MyChats/>}
         {user && <ChatBox/>}
       </Box>
